8ce6972b29452f3be76ce8fa154c0631471b5543
Richard Lawson <51132990+lawsonarichard@users.noreply.github.com>
2020-10-16 14:35:42 -0700

    startingState
    set startingState with public methods

--- Assets/State.cs
+++ Assets/State.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [ TextArea(10, 14)] [SerializeField] string storyText;
+    [TextArea(10, 14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 }
